69cae429df476536ea40f13e6e0c05b97a6773f2
thatcalledxiaoren <lihaoqian12@outlook.com>
2025-06-23 13:35:07 +0800

    1`112
    21`3`1

--- graduate/1.cpp
+++ graduate/1.cpp
@@ -5,22 +5,27 @@
 using namespace std;
 int main(){
 	char nigger[1212];
-	for(int i=1;i<=54;i++){
-		string a=to_string(i)+".html";
-		strcpy(nigger,a.c_str());
-		freopen(nigger,"w",stdout);
-		cout<<"<!DOCTYPE html>\n";
-		cout<<"<html>\n";
-		cout<<"<head>\n";
-		cout<<"<meta charset=\"gb18030\">\n";
-		cout<<"</head>\n";
-		cout<<"<body>\n";
-		cout<<"<h1>学号："<<to_string(i);
-		cout<<"</h1>\n<br><strong>剩下的需要5345431年才可编辑完成。</strong>\n";
-		cout<<"\n<br><a href='/ces'>玩会消极赤沙为嘛：<img src='/logo.png'></a>\n";
-		cout<<"</body>\n";
-		cout<<"</html>\n";
-		fclose(stdout);
+	for(int i=2;i<=54;i++){
+		string a=to_string(i-1)+".html";
+		string b=to_string(i)+".html";
+		//These are my failure -- most browser don't know what is GB18030!!
+//		freopen(nigger,"w",stdout);
+//		cout<<"<!DOCTYPE html>\n";
+//		cout<<"<html>\n";
+//		cout<<"<head>\n";
+//		cout<<"<meta charset=\"gb18030\">\n";
+//		cout<<"</head>\n";
+//		cout<<"<body>\n";
+//		cout<<"<h1>学号："<<to_string(i);
+//		cout<<"</h1>\n<br><strong>剩下的需要5345431年才可编辑完成。</strong>\n";
+//		cout<<"\n<br><a href='/ces'>玩会消极赤沙为嘛：<img src='/logo.png'></a>\n";
+//		cout<<"</body>\n";
+//		cout<<"</html>\n";
+//		fclose(stdout);
+		//Real nuts.
+		string c="copy "+a+" "+b;
+		strcpy(nigger,c.c_str());
+		system(nigger);
 	}
 	return 0;
 }
